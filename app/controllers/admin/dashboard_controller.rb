997ad9951cffe5b5f8150bde62a0c8978f06a634
Alex Cheung <alexc.sfu@gmail.com>
2022-06-30 02:28:24 +0000

    added environment variables for security purpose, also updated .env.example for demonstration how to set up environment variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: "Jungle", password: "book"
-  
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
