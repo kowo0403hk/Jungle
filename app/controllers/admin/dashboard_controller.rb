27bb2ed5535f05b2389027d73f72302f234a43a0
Alex Cheung <alexc.sfu@gmail.com>
2022-06-30 05:14:26 +0000

    added active record search for products and categories count. Also implemented dashboard table to display the counts through iteration.'

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
 
   def show
+    products = Product.all.count
+    categories = Category.all.count
+    @all_count = {categories:, products:}
+    # we do all the necessary search in the backend first, then put them into a hash so that the front end can iterate through it for table rendering
   end
 end
