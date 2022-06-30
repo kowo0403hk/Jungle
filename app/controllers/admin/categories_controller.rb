443107074889e68066e2f85822b63b6a0af99bef
Alex Cheung <alexc.sfu@gmail.com>
2022-06-30 19:07:43 +0000

    updated admin/categories_controller and _top_nav for pathing

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,29 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
+
   def index
+    @categories = Category.find(id: :asc).all
   end
 
   def new
+    @category = Category.new
   end
+
+  def created
+    @category = Category.new(category_params)
+
+    if @categoy.save
+      redirect_to [:admin, :categories], notice: 'Category was successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
