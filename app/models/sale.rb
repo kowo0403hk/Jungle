8cfaad894934c31683e0cdee7626f2281d1a7901
Alex Cheung <alexc.sfu@gmail.com>
2022-07-01 04:02:28 +0000

    moved the conditionals (business logic) to the model folder

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,14 @@
 class Sale < ApplicationRecord
   def finished?
-    self.ends_on < Date.current
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
   end
 
 end
