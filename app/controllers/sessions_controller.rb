d9062800261e05ce2353e059f1e4ec6819a8b531
Alex Cheung <alexc.sfu@gmail.com>
2022-07-01 00:45:58 +0000

    updated loging and logout logic in the sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    # the authenticate method is from the bcrypt library
+    if user && user.authenticate(params[:password])
+
+      #if password matches, then we use assign cookies to this user and login him/her in
+      session[:user_id] = user.id
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/sessions/new"
   end
 end
