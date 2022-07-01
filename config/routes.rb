e9674bdbf1f41523ba5e7d88d5e2b4f520863e78
Alex Cheung <alexc.sfu@gmail.com>
2022-07-01 00:53:15 +0000

    edited the _top_nav partial to show the user login and user registration options

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
