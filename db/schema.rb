e01c163adef787826569eb6b2a15808a9901d358
Alex Cheung <alexc.sfu@gmail.com>
2022-07-04 22:10:16 +0000

    deleted widget model and its test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_215307) do
+ActiveRecord::Schema.define(version: 2022_07_01_030533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,11 +67,6 @@ ActiveRecord::Schema.define(version: 2022_07_04_215307) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "widgets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
