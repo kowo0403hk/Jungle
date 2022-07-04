9858b5a431270dfc5d15994479e9850610e4d750
Alex Cheung <alexc.sfu@gmail.com>
2022-07-04 23:26:59 +0000

    consulted mentor and changed approach of testing of product model. Instead of repeatedly testing if an input contains a certain field, I changed to set one of the mandatory field as nil and check the input is invalid and also its error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,56 +5,57 @@ RSpec.describe Product, type: :model do
     it 'validates the existence of the name of a product' do
       @category = Category.create(name:'Test_Category')
       @product = Product.new
-      @product.name = "Test_Product"
-      @product.price = 2999
+      @product.name = nil
+      @product.price_cents = 2999
       @product.quantity = 2
       @product.category_id = @category.id
-      @product.save
-      expect(@product.name).to be_present
+      # using the be_valid to check if the input of a variable is valid based on it's model validation rules
+      expect(@product).not_to be_valid
+      # to test if a particular error message exists pertaining to the nil input we made previously
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'validates the existence of the price of a product' do
       @category = Category.create(name:'Test_Category')
       @product = Product.new
       @product.name = "Test_Product"
-      @product.price = 2999
+      @product.price_cents = nil
       @product.quantity = 2
       @product.category_id = @category.id
-      @product.save
-      expect(@product.price).to be_present
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'validates the existence of the quantity of a product' do
       @category = Category.create(name:'Test_Category')
       @product = Product.new
       @product.name = "Test_Product"
-      @product.price = 2999
-      @product.quantity = 0
+      @product.price_cents = 2999
+      @product.quantity = nil
       @product.category_id = @category.id
-      @product.save
-      expect(@product.quantity).to be_present
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'validates the existence of the category_id of a product' do
       @category = Category.create(name:'Test_Category')
       @product = Product.new
       @product.name = "Test_Product"
-      @product.price = 2999
+      @product.price_cents = 2999
       @product.quantity = 2
-      @product.category_id = @category.id
-      @product.save
-      expect(@product.category_id).to be_present
+      @product.category_id = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
-    it ' provides error message will be generated if the required fields are missing' do
+    it 'validates a valid input' do
       @category = Category.create(name:'Test_Category')
       @product = Product.new
-      @product.name = nil
-      @product.price = nil
-      @product.quantity = nil
-      @product.category_id = nil
-      @product.save
-      expect(@product.errors.full_messages).to be_present
+      @product.name = "Test_Product"
+      @product.price_cents = 2999
+      @product.quantity = 2
+      @product.category_id = @category.id
+      expect(@product).to be_valid
     end
   end
 end
