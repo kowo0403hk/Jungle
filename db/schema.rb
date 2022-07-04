f3ecf0ab048d53adad6a2604c5bec1bd28e8fddd
Alex Cheung <alexc.sfu@gmail.com>
2022-07-04 21:55:52 +0000

    added widget model and its spec file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_215718) do
+ActiveRecord::Schema.define(version: 2022_07_04_215307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,25 @@ ActiveRecord::Schema.define(version: 2022_06_30_215718) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "sales", force: :cascade do |t|
+    t.string "name"
+    t.integer "percent_off"
+    t.date "starts_on"
+    t.date "ends_on"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
